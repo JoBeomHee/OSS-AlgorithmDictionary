32108bd2e6ee0a9438acc0efe3883540f5b0ecfc
JoBumHee <JoBumHee@DESKTOP-L991RB1>
2016-11-12 16:46:08 +0900

    Algorithm Menu 수정해서 올립니다.

--- Algorithm Dictionary/AlgorithmMenu.Designer.cs
+++ Algorithm Dictionary/AlgorithmMenu.Designer.cs
@@ -28,240 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_stack = new System.Windows.Forms.Button();
-            this.btn_LinkedList = new System.Windows.Forms.Button();
-            this.btn_queue = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.btn_InsertSort = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
-            this.button11 = new System.Windows.Forms.Button();
-            this.button12 = new System.Windows.Forms.Button();
-            this.button13 = new System.Windows.Forms.Button();
-            this.button14 = new System.Windows.Forms.Button();
-            this.button15 = new System.Windows.Forms.Button();
-            this.button16 = new System.Windows.Forms.Button();
-            this.button17 = new System.Windows.Forms.Button();
-            this.button18 = new System.Windows.Forms.Button();
+            this.tb_Search = new System.Windows.Forms.TextBox();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.lb_menu = new System.Windows.Forms.ListBox();
+            this.pb_Main = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Main)).BeginInit();
             this.SuspendLayout();
             // 
-            // btn_stack
+            // tb_Search
             // 
-            this.btn_stack.Location = new System.Drawing.Point(26, 27);
-            this.btn_stack.Name = "btn_stack";
-            this.btn_stack.Size = new System.Drawing.Size(121, 53);
-            this.btn_stack.TabIndex = 0;
-            this.btn_stack.Text = "스택";
-            this.btn_stack.UseVisualStyleBackColor = true;
-            this.btn_stack.Click += new System.EventHandler(this.btn_stack_Click);
+            this.tb_Search.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tb_Search.Location = new System.Drawing.Point(12, 12);
+            this.tb_Search.Name = "tb_Search";
+            this.tb_Search.Size = new System.Drawing.Size(307, 29);
+            this.tb_Search.TabIndex = 18;
             // 
-            // btn_LinkedList
+            // btn_Search
             // 
-            this.btn_LinkedList.Location = new System.Drawing.Point(171, 27);
-            this.btn_LinkedList.Name = "btn_LinkedList";
-            this.btn_LinkedList.Size = new System.Drawing.Size(121, 53);
-            this.btn_LinkedList.TabIndex = 1;
-            this.btn_LinkedList.Text = "링크드리스트";
-            this.btn_LinkedList.UseVisualStyleBackColor = true;
-            this.btn_LinkedList.Click += new System.EventHandler(this.btn_LinkedList_Click);
+            this.btn_Search.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Search.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_Search.Location = new System.Drawing.Point(325, 12);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(97, 29);
+            this.btn_Search.TabIndex = 19;
+            this.btn_Search.Text = "검색";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            this.btn_Search.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Key_Enter);
             // 
-            // btn_queue
+            // lb_menu
             // 
-            this.btn_queue.Location = new System.Drawing.Point(323, 27);
-            this.btn_queue.Name = "btn_queue";
-            this.btn_queue.Size = new System.Drawing.Size(121, 53);
-            this.btn_queue.TabIndex = 2;
-            this.btn_queue.Text = "큐";
-            this.btn_queue.UseVisualStyleBackColor = true;
+            this.lb_menu.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lb_menu.FormattingEnabled = true;
+            this.lb_menu.ItemHeight = 19;
+            this.lb_menu.Items.AddRange(new object[] {
+            "스택",
+            "링크드리스트",
+            "큐",
+            "크루스칼 알고리즘",
+            "프림 알고리즘",
+            "깊이우선탐색",
+            "너비우선탐색",
+            "트리",
+            "삽입정렬",
+            "선택정렬",
+            "쉘정렬",
+            "버블정렬",
+            "힙정렬",
+            "퀵정렬",
+            "카운팅정렬",
+            "기수정렬",
+            "버킷정렬",
+            "머지정렬"});
+            this.lb_menu.Location = new System.Drawing.Point(12, 52);
+            this.lb_menu.Name = "lb_menu";
+            this.lb_menu.Size = new System.Drawing.Size(410, 232);
+            this.lb_menu.TabIndex = 20;
             // 
-            // button4
+            // pb_Main
             // 
-            this.button4.Location = new System.Drawing.Point(26, 105);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(121, 53);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "크루스칼 알고리즘";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(171, 105);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(121, 53);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "프림 알고리즘";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(323, 105);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(121, 53);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "깊이우선탐색";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(26, 181);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(121, 53);
-            this.button7.TabIndex = 6;
-            this.button7.Text = "너비우선탐색";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(171, 181);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(121, 53);
-            this.button8.TabIndex = 7;
-            this.button8.Text = "트리";
-            this.button8.UseVisualStyleBackColor = true;
-            // 
-            // btn_InsertSort
-            // 
-            this.btn_InsertSort.Location = new System.Drawing.Point(323, 181);
-            this.btn_InsertSort.Name = "btn_InsertSort";
-            this.btn_InsertSort.Size = new System.Drawing.Size(121, 53);
-            this.btn_InsertSort.TabIndex = 8;
-            this.btn_InsertSort.Text = "삽입정렬";
-            this.btn_InsertSort.UseVisualStyleBackColor = true;
-            this.btn_InsertSort.Click += new System.EventHandler(this.btn_InsertSort_Click);
-            // 
-            // button10
-            // 
-            this.button10.Location = new System.Drawing.Point(26, 266);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(121, 53);
-            this.button10.TabIndex = 9;
-            this.button10.Text = "선택정렬";
-            this.button10.UseVisualStyleBackColor = true;
-            // 
-            // button11
-            // 
-            this.button11.Location = new System.Drawing.Point(171, 266);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(121, 53);
-            this.button11.TabIndex = 10;
-            this.button11.Text = "쉘 정렬";
-            this.button11.UseVisualStyleBackColor = true;
-            // 
-            // button12
-            // 
-            this.button12.Location = new System.Drawing.Point(323, 266);
-            this.button12.Name = "button12";
-            this.button12.Size = new System.Drawing.Size(121, 53);
-            this.button12.TabIndex = 11;
-            this.button12.Text = "버블정렬";
-            this.button12.UseVisualStyleBackColor = true;
-            // 
-            // button13
-            // 
-            this.button13.Location = new System.Drawing.Point(26, 343);
-            this.button13.Name = "button13";
-            this.button13.Size = new System.Drawing.Size(121, 53);
-            this.button13.TabIndex = 12;
-            this.button13.Text = "힙정렬";
-            this.button13.UseVisualStyleBackColor = true;
-            // 
-            // button14
-            // 
-            this.button14.Location = new System.Drawing.Point(171, 343);
-            this.button14.Name = "button14";
-            this.button14.Size = new System.Drawing.Size(121, 53);
-            this.button14.TabIndex = 13;
-            this.button14.Text = "퀵정렬";
-            this.button14.UseVisualStyleBackColor = true;
-            // 
-            // button15
-            // 
-            this.button15.Location = new System.Drawing.Point(323, 343);
-            this.button15.Name = "button15";
-            this.button15.Size = new System.Drawing.Size(121, 53);
-            this.button15.TabIndex = 14;
-            this.button15.Text = "카운팅정렬";
-            this.button15.UseVisualStyleBackColor = true;
-            // 
-            // button16
-            // 
-            this.button16.Location = new System.Drawing.Point(323, 417);
-            this.button16.Name = "button16";
-            this.button16.Size = new System.Drawing.Size(121, 53);
-            this.button16.TabIndex = 15;
-            this.button16.Text = "머지정렬";
-            this.button16.UseVisualStyleBackColor = true;
-            // 
-            // button17
-            // 
-            this.button17.Location = new System.Drawing.Point(171, 417);
-            this.button17.Name = "button17";
-            this.button17.Size = new System.Drawing.Size(121, 53);
-            this.button17.TabIndex = 16;
-            this.button17.Text = "버킷정렬";
-            this.button17.UseVisualStyleBackColor = true;
-            // 
-            // button18
-            // 
-            this.button18.Location = new System.Drawing.Point(26, 417);
-            this.button18.Name = "button18";
-            this.button18.Size = new System.Drawing.Size(121, 53);
-            this.button18.TabIndex = 17;
-            this.button18.Text = "기수정렬";
-            this.button18.UseVisualStyleBackColor = true;
+            this.pb_Main.Image = global::Algorithm_Dictionary.Properties.Resources.AlgorithmDictionary_아이콘;
+            this.pb_Main.Location = new System.Drawing.Point(428, 12);
+            this.pb_Main.Name = "pb_Main";
+            this.pb_Main.Size = new System.Drawing.Size(148, 125);
+            this.pb_Main.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_Main.TabIndex = 21;
+            this.pb_Main.TabStop = false;
             // 
             // AlgorithmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(475, 496);
-            this.Controls.Add(this.button18);
-            this.Controls.Add(this.button17);
-            this.Controls.Add(this.button16);
-            this.Controls.Add(this.button15);
-            this.Controls.Add(this.button14);
-            this.Controls.Add(this.button13);
-            this.Controls.Add(this.button12);
-            this.Controls.Add(this.button11);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.btn_InsertSort);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.btn_queue);
-            this.Controls.Add(this.btn_LinkedList);
-            this.Controls.Add(this.btn_stack);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(588, 304);
+            this.Controls.Add(this.pb_Main);
+            this.Controls.Add(this.lb_menu);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.tb_Search);
+            this.DoubleBuffered = true;
             this.Name = "AlgorithmMenu";
             this.Text = "알고리즘 종류";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Main)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btn_stack;
-        private System.Windows.Forms.Button btn_LinkedList;
-        private System.Windows.Forms.Button btn_queue;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button btn_InsertSort;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Button button11;
-        private System.Windows.Forms.Button button12;
-        private System.Windows.Forms.Button button13;
-        private System.Windows.Forms.Button button14;
-        private System.Windows.Forms.Button button15;
-        private System.Windows.Forms.Button button16;
-        private System.Windows.Forms.Button button17;
-        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.TextBox tb_Search;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.ListBox lb_menu;
+        private System.Windows.Forms.PictureBox pb_Main;
     }
 }
